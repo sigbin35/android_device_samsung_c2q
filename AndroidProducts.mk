8a8eca2c43d5644bc353483a8e925f911eab9dd1
sigbin35 <jakemercadoty@gmail.com>
2025-08-03 08:22:41 +0800

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,3 +1,4 @@
+
 #
 # Copyright (C) 2024 The Android Open Source Project
 # Copyright (C) 2024 SebaUbuntu's TWRP device tree generator
@@ -6,9 +7,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/omni_c2q.mk
+    $(LOCAL_DIR)/lineage_c2q.mk
 
 COMMON_LUNCH_CHOICES := \
-    omni_c2q-user \
-    omni_c2q-userdebug \
-    omni_c2q-eng
+    lineage_c2q-user \
+    lineage_c2q-userdebug \
+    lineage_c2q-eng
