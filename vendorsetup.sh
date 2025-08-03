a30447ab5220c54b93221510dfcd8234a2dd6dc6
sigbin35 <jakemercadoty@gmail.com>
2025-08-03 08:46:08 +0800

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -5,6 +5,6 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-add_lunch_combo omni_c2q-user
-add_lunch_combo omni_c2q-userdebug
-add_lunch_combo omni_c2q-eng
+add_lunch_combo twrp_c2q-user
+add_lunch_combo twrp_c2q-userdebug
+add_lunch_combo twrp_c2q-eng
